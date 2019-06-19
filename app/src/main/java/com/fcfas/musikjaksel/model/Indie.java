24690d6648cffa478d64c90883d300bba5e09264
Alif Nur Rachman <alifnurrachman907@gmail.com>
2019-06-20 05:25:48 +0700

    add model

---
+++ app/src/main/java/com/fcfas/musikjaksel/model/Indie.java
@@ -0,0 +1,45 @@
+package com.fcfas.musikjaksel.model;
+
+public class Indie {
+    private String name, category, remarks, photo, linkcontent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getLinkcontent() {
+        return linkcontent;
+    }
+
+    public void setLinkcontent(String linkcontent) {
+        this.linkcontent = linkcontent;
+    }
+}
