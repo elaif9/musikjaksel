c99b1e7791d8ff01c19f18e6a205c92fd0564430
Alif Nur Rachman <alifnurrachman907@gmail.com>
2019-06-20 11:01:29 +0700

    add menu

--- app/src/main/java/com/fcfas/musikjaksel/MainActivity.java
+++ app/src/main/java/com/fcfas/musikjaksel/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.fcfas.musikjaksel.adapter.ListIndieAdapter;
 import com.fcfas.musikjaksel.model.Indie;
@@ -32,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
         ListIndieAdapter listIndieAdapter = new ListIndieAdapter(this);
         listIndieAdapter.setListIndie(list);
         rvCategory.setAdapter(listIndieAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                break;
+            case R.id.action_gallery;
+                break;
+            case R.id.action_story:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
